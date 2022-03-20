97617f412b5ecc487e75b3011ecf3ee9489db26f
Ibugani <101966173+Ibugani@users.noreply.github.com>
2022-03-20 13:01:33 +0530

    Add files via upload

--- Mobile.java
+++ Mobile.java
@@ -1,20 +1,30 @@
 package week1.day1;
 
 public class Mobile {
-	public void makecall() {
-		System.out.println("makecall");
+	String Model = "Apple";
+	float Weight = 0.25F;
+	boolean fullyCharged = true;
+	int Cost = 25000;
+		
+	public void makeCall() {
+	
+		System.out.println("Make Call");
 	}
 
 	public void sendMsg() {
 		System.out.println("sendMsg");
 	}
+	
 	public static void main(String[] args) {
-		
-System.out.println("Mobile");
 Mobile myMobile = new Mobile();
-myMobile.makecall();
+myMobile.makeCall();
 myMobile.sendMsg();
+System.out.println(myMobile.Model);
+System.out.println(myMobile.Weight);
+System.out.println(myMobile.fullyCharged);
+System.out.println(myMobile.Cost);
 
 	}
 
 }
+
